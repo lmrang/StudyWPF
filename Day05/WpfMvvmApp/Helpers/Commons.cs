915bbf92b26b0a9e14aea45a962934dac7efd3cb
lmrang <limlr@naver.com>
2022-06-07 12:47:49 +0900

    220607 MVVM 아침 커밋

---
+++ Day05/WpfMvvmApp/Helpers/Commons.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfMvvmApp.Helpers
+{
+    public class Commons
+    {
+        /// <summary>
+        /// 이메일 검증 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>True 이메일주소형태 아님/False 이메일주소형태가 맞음</returns>
+        public static bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
+            //verbatim string
+        }
+        /// <summary>
+        /// 나이계산
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int CalcAge(DateTime value)
+        {
+            int middle;
+            if (DateTime.Now.Month < value.Month || DateTime.Now.Month == value.Month &&
+                DateTime.Now.Day < value.Day)
+                middle = DateTime.Now.Year - value.Year - 1;
+            else
+                middle = DateTime.Now.Year - value.Year;
+
+            return middle;
+        }
+    }
+}
---
+++ Day05/WpfMvvmApp/Models/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using WpfMvvmApp.Helpers;
+
+namespace WpfMvvmApp.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string email;
+        public string Email
+        {//대문자는 속성
+            get { return email; }       //get => email; 과 동일
+            set
+            {
+                if (Commons.IsValidEmail(email) == true)
+                    throw new Exception("Invaild Email");
+                else
+                    email = value;
+            }
+        }
+        public DateTime date;
+        public DateTime Date 
+        { 
+            get { return date; }
+            set
+            {
+                var result = Commons.CalcAge(value);
+                if (result > 135 || result < 0)
+                    throw new Exception("Invalid Date");
+                else
+                    date = value;
+            }
+        }
+        public bool IsBirthday 
+        { 
+            get
+            {
+                return DateTime.Now.Month == Date.Month &&
+                    DateTime.Now.Day == Date.Day;
+            }
+        }
+        public bool IsAdult 
+        { 
+            get
+            {
+                return Commons.CalcAge(Date) > 18;
+            }
+        }
+
+        public Person(string firstName, string lastName, string email, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Date = date;
+        }
+    }
+}
