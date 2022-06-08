df649f5e0cbc16acd4aa55c02c78e6d735c0955c
lmrang <limlr@naver.com>
2022-06-08 17:43:46 +0900

    220608 커밋

---
+++ Day06/DummyDataTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyDataTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new SampleRepository();
+            var customers = repository.GetCustomers();
+
+            //Console.WriteLine(JsonConvert.SerializeObject(customers, Formatting.Indented));
+            //Console.WriteLine(customers);
+        }
+    }
+}
